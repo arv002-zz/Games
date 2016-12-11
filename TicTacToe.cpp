fdf9ccdcbbfafdfa1aa44d08f27d5d494fbe8343
Arvind Rajpurohit <arvindiitdhn@gmail.com>
2016-12-11 21:04:13 +0530

    Created an array for board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,8 +3,27 @@ using namespace std;
 
 char board[10]={'0','1','2','3','4','5','6','7','8','9'};
 
+void board_status()
+{
+   cout<< endl<<"Get ready to play tic tac toe"<<endl<<endl;
+
+   cout<<"Player 1:- X"<<"  "<<"Player 2:- O"<<endl<<endl;
+
+   cout<<"      |       |       "<<endl;
+
+   cout<<"______|_______|_______"<<endl;
+
+   cout<<"      |       |       "<<endl;
+
+   cout<<"______|_______|_______"<<endl;
+
+   cout<<"      |       |       "<<endl;
+
+   cout<<"      |       |       "<<endl;
+
+}
 int main()
 {
 
-
+  board_status();
 }
