d0d62aed9b1ccbb8527dd302793e56f23987ce16
Arvind Rajpurohit <arvindiitdhn@gmail.com>
2016-12-13 04:59:11 +0530

    taking input and showing board status

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -11,19 +11,47 @@ void board_status()
 
    cout<<"      |       |       "<<endl;
 
-   cout<<"______|_______|_______"<<endl;
-
-   cout<<"      |       |       "<<endl;
+   cout<<"     "<<board[1]<<"|"<<"      "<<board[2]<<"|"<<"     "<<board[3]<<endl;
 
    cout<<"______|_______|_______"<<endl;
 
    cout<<"      |       |       "<<endl;
+
+   cout<<"     "<<board[4]<<"|"<<"      "<<board[5]<<"|"<<"     "<<board[6]<<endl;
+
+   cout<<"______|_______|_______"<<endl;
+
+   cout<<"      |       |       "<<endl;
+
+   cout<<"     "<<board[7]<<"|"<<"      "<<board[8]<<"|"<<"     "<<board[9]<<endl;
 
    cout<<"      |       |       "<<endl<<endl;
 
 }
 int main()
 {
+  int i=0,player=1,box_no;
+  do
+    {
+      board_status();
+      player=player%2? 1 : 2;
+      cout<<"Enter the no. to cross, Player "<<player<<":-"<<endl;
+      cin>>box_no;
 
-  board_status();
+      if(player==1 && board[box_no]!='X' && board[box_no]!='O')
+          board[box_no]='X';
+
+      else if(player==2 && board[box_no]!='X' && board[box_no]!='O')
+          board[box_no]='O';
+
+      else
+        cout<<"Wrong No.";
+
+      board_status();
+
+     player++;
+    }
+  while(i==-1);
+
+  return 0;
 }
