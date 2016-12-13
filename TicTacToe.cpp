5a476396311c9be01ae4f4387fd254016682b5bd
Arvind Rajpurohit <arvindiitdhn@gmail.com>
2016-12-13 06:22:45 +0530

    Cleaner Code

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,97 +4,102 @@ using namespace std;
 char board[10]={'0','1','2','3','4','5','6','7','8','9'};
 
 void board_status()
-{
-   cout<< endl<<"Get ready to play tic tac toe"<<endl<<endl;
+       {
 
-   cout<<"Player 1:- X"<<"  "<<"Player 2:- O"<<endl<<endl;
+               cout<< endl<<"Get ready to play tic tac toe"<<endl<<endl;
 
-   cout<<"      |       |       "<<endl;
+               cout<<"Player 1:- X"<<"  "<<"Player 2:- O"<<endl<<endl;
 
-   cout<<"     "<<board[1]<<"|"<<"      "<<board[2]<<"|"<<"     "<<board[3]<<endl;
+               cout<<"      |       |       "<<endl;
 
-   cout<<"______|_______|_______"<<endl;
+               cout<<"     "<<board[1]<<"|"<<"      "<<board[2]<<"|"<<"     "<<board[3]<<endl;
 
-   cout<<"      |       |       "<<endl;
+               cout<<"______|_______|_______"<<endl;
 
-   cout<<"     "<<board[4]<<"|"<<"      "<<board[5]<<"|"<<"     "<<board[6]<<endl;
+               cout<<"      |       |       "<<endl;
 
-   cout<<"______|_______|_______"<<endl;
+               cout<<"     "<<board[4]<<"|"<<"      "<<board[5]<<"|"<<"     "<<board[6]<<endl;
 
-   cout<<"      |       |       "<<endl;
+               cout<<"______|_______|_______"<<endl;
 
-   cout<<"     "<<board[7]<<"|"<<"      "<<board[8]<<"|"<<"     "<<board[9]<<endl;
+               cout<<"      |       |       "<<endl;
 
-   cout<<"      |       |       "<<endl<<endl;
+               cout<<"     "<<board[7]<<"|"<<"      "<<board[8]<<"|"<<"     "<<board[9]<<endl;
+
+               cout<<"      |       |       "<<endl<<endl;
 
 
-}
+       }
 
 int Check_win()
-{
+       {
 
-    if(board[1]==board[2] && board[2]==board[3])
-        return 1;
-    else if(board[1]==board[4] && board[4]==board[7])
-        return 1;
-    else if(board[1]==board[5] && board[5]==board[9])
-        return 1;
-    else if(board[2]==board[5] && board[5]==board[8])
-        return 1;
-    else if(board[3]==board[6] && board[6]==board[9])
-        return 1;
-    else if(board[3]==board[5] && board[5]==board[7])
-        return 1;
-    else if(board[4]==board[5] && board[5]==board[6])
-        return 1;
-    else if(board[7]==board[8] && board[8]==board[9])
-        return 1;
-    else if (board[1]!='1' && board[2]!='2' && board[3]!='3' &&
-          board[4]!='4' && board[5]!='5' && board[6]!='6' &&
-          board[7]!='7' && board[8]!='8' && board[9]!='9')
-    return 0;
+                if(board[1]==board[2] && board[2]==board[3])
+                    return 1;
+                else if(board[1]==board[4] && board[4]==board[7])
+                    return 1;
+                else if(board[1]==board[5] && board[5]==board[9])
+                    return 1;
+                else if(board[2]==board[5] && board[5]==board[8])
+                    return 1;
+                else if(board[3]==board[6] && board[6]==board[9])
+                    return 1;
+                else if(board[3]==board[5] && board[5]==board[7])
+                    return 1;
+                else if(board[4]==board[5] && board[5]==board[6])
+                    return 1;
+                else if(board[7]==board[8] && board[8]==board[9])
+                    return 1;
+                else if (board[1]!='1' && board[2]!='2' && board[3]!='3' &&
+                      board[4]!='4' && board[5]!='5' && board[6]!='6' &&
+                      board[7]!='7' && board[8]!='8' && board[9]!='9')
+                return 0;
 
-    return -1;
-}
+                return -1;
+       }
+
 int main()
-{
-  int i=0,player=1,box_no;
-  do
-    {
-      board_status();
+      {
 
-      player=player%2? 1 : 2;
+              int i=0,player=1,box_no;
 
-      cout<<"Enter the no. to cross, Player "<<player<<":-"<<endl;
+              do
+                {
+                      board_status();
 
-      cin>>box_no;
+                      player=player%2? 1 : 2;
 
-      if(player==1 && board[box_no]!='X' && board[box_no]!='O')
-          board[box_no]='X';
+                      cout<<"Enter the no. to cross, Player "<<player<<":-"<<endl;
 
-      else if(player==2 && board[box_no]!='X' && board[box_no]!='O')
-          board[box_no]='O';
+                      cin>>box_no;
 
-      else
-        {
-          cout<<"Wrong No.";
-          player--;
-        }
+                      if(player==1 && board[box_no]!='X' && board[box_no]!='O')
+                          board[box_no]='X';
 
-      i=Check_win();
+                      else if(player==2 && board[box_no]!='X' && board[box_no]!='O')
+                          board[box_no]='O';
+
+                      else
+                        {
+                          cout<<"Wrong No.";
+                          player--;
+                        }
+
+                      i=Check_win();
 
 
-     player++;
-    }
+                     player++;
+                }
 
-  while(i==-1);
+              while(i==-1);
 
-   board_status();
+              board_status();
 
-    if(i==1)
-    cout<<"Player "<<player<<" Won!!"<<endl;
-    else cout<<"Draw";
+              if(i==1)
+              cout<<"Player "<<player<<" Won!!"<<endl;
 
-    return 0;
+              else cout<<"Draw";
+
+              return 0;
 
 }
